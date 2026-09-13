3b12b6d8460dbbfab3a07e370771c7ffa4d83f1a
babulpro <sh8657706@gmail.com>
2026-09-14 00:03:32 +0600

    overall some update according to ui

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -6,11 +6,11 @@ const Footer = () => {
     <footer className="bg-slate-100 text-slate-400">
       <div className="mx-auto max-w-7xl px-4 py-14">
 
-        <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-5">
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-5">
 
           {/* Brand */}
           <div className="lg:col-span-2 ">
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 w-20 h-20">
               <img src={myImage} alt="logo image of the dec Stack" className="m-auto md:m-0"/>
                 
             </div>
@@ -21,7 +21,7 @@ modern software.
             </p>
 
             {/* Social Links */}
-            <div className="mt-6 flex gap-4 justify-center md:justify-start">
+            <div className="mt-6 flex gap-4 text-slate-900 justify-center md:justify-start">
               <a
                 href="#"
                 className="transition hover:text-slate-500"
